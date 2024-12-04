53eb164b13ae5e430e4f14abb244414091e00818
john-og <john.j.ogundeji@gmail.com>
2024-12-04 09:06:47 -0800

    add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import ParticleNetwork from './components/ParticleNetwork';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Experience from './pages/Experience';
 import Education from './pages/Education';
 import Projects from './pages/Projects';
+import ReactGA from 'react-ga4';
+
+
+// Initialize GA outside of the component
+ReactGA.initialize('G-FS8F3BL959');
+
+// Create a component to track page views
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
 
 function App() {
   return (
